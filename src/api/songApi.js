f00d9229d76f385c24543357d452138fb841a7ea
Suiseich <aarontayag89@gmail.com>
2026-04-23 12:00:21 +0800

    Updated songApi.js to match render

--- src/api/songApi.js
+++ src/api/songApi.js
@@ -1,8 +1,9 @@
-import axios from "axios";
+// src/api/axios.js (or wherever you set up axios)
+import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || "/api/tayag/songs";
+const api = axios.create({
+  // Use your actual backend URL here
+  baseURL: 'https://song-api-fy89.onrender.com/tayag' 
+});
 
-export const getSongs = async () => {
-  const res = await axios.get(API_URL);
-  return res.data;
-};
+export const getSongs = () => api.get('/songs');
